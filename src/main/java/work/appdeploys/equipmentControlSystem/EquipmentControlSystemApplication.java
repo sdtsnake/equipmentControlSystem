c7df864421a27cef11b85138f38d418ce3b9e9fa
Juan Eslava <juan.eslava>
2022-08-18 21:35:32 -0500

    arquetipos proyecto

---
+++ src/main/java/work/appdeploys/equipmentControlSystem/EquipmentControlSystemApplication.java
@@ -0,0 +1,13 @@
+package work.appdeploys.equipmentControlSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/appdeploys/equipmentControlSystem/EquipmentControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package work.appdeploys.equipmentControlSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
