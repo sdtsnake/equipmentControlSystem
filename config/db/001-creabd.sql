d68d117cc681b8ddb1ad884e672092814b4541b1
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2022-08-20 21:10:52 -0500

    configuracion de la base datos completa.
    se realiza configuracion completa de la base de datos
    con contenedores
    
    		# Please enter the commit message for your changes. Lines starting

--- config/db/001-creabd.sql
+++ config/db/001-creabd.sql
@@ -1,5 +1,9 @@
 create table orders(id serial primary key, model varchar(500), nro_serial varchar(500),asset varchar(500),issue varchar(500),insident int, note varchar(1000), status_order char(50), idusercreate int, datacreate date, nro_order int);
 create table shcool(id serial primary key, descripcion varchar(500) not null);
-create table user(id serial primary key,  email varchar(250) not null, passwd varchar(500) not null, descripcion varchar(500) not null, rol char(1));
+create table users(id serial primary key,  email varchar(250) not null, passwd varchar(500) not null, descripcion varchar(500) not null, rol char(1));
 create table orderbyshcool(idorder int, idshcool int);
+
+ alter table orders add constraint FK_orders_users foreign key (idusercreate) references users(id);
+ alter table orderbyshcool add constraint FK_orders_shcool foreign key (idorder) references orders(id);
+ alter table orderbyshcool add constraint FK_shcool_orders foreign key (idshcool) references shcool(id);
 
